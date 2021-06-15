9eda81957a1426f629eabc83b6137911df996808
Simon de Vries <simon.vries@stud.h-da.de>
2021-06-15 15:32:18 +0200

    Commentaries and simplification

--- 01_Javascript/TextAnimation.js
+++ 01_Javascript/TextAnimation.js
@@ -20,11 +20,17 @@ export default class TextAnimation {
         noStroke();
         textFont(this.textFont);
         textSize(this.textSize);
+
+        //substring:
+        //string is like an array
+        //span between first array-index (0) and second array-index (this.n + 1)
+        //like random function: random(0, this.n + 1)
         text(this.firstSentence.substring(0, this.n + 1), this.x, this.y);
         this.n++;
         this.typeSpeed = false;
       }
-      //So that it doesnt disappear, when every string is finished and n is equal to the string length
+      //With this function the text doesnt disappears
+      // when every string is completed and n is equal to the string length
       if (this.n === this.firstSentence.length) {
         fill(this.textColor);
         noStroke();
@@ -32,24 +38,29 @@ export default class TextAnimation {
         textSize(this.textSize);
         text(this.firstSentence.substring(0, this.n + 1), this.x, this.y);
       }
-
+      //typing ends, when every letter is displayed
       if (this.n === this.firstSentence.length) {
         this.typingEnded = true;
       }
     } else {
-      //during typeSpeed is false, it is blacked out. During this time it displays the previous status
+      //when typeSpeed is false, the text is blacked out for this time.
+      //So this function displays the currently text when typeSpeed=false
       fill(this.textColor);
       noStroke();
       textFont(this.textFont);
       textSize(this.textSize);
       text(this.firstSentence.substring(0, this.n + 1), this.x, this.y);
     }
+
+    //speed, how fast the next letter is displayed
     this.counter += this.speed;
     if (this.counter > 50) {
       this.counter = 0;
       this.typeSpeed = true;
     }
   }
+
+  //use for mouseClicked
   displayAllTextAtOnce() {
     this.n = this.firstSentence.length;
   }
--- 01_Javascript/TextAnimationWithMultipleSentences.js
+++ 01_Javascript/TextAnimationWithMultipleSentences.js
@@ -23,12 +23,16 @@ export default class TextAnimationWithMultipleSentences {
   ) {
     this.x = x;
     this.y = y;
+    //space between text rows
     this.spacing = spacing;
 
+    //the first two rows could be written in "duper-bold" and not "duper",
+    //so the possibility to make them bold is added (firstSenType/secondSenType)
     this.firstSen = firstSen;
     this.firstSenType = firstSenType;
     this.secondSen = secondSen;
     this.secondSenType = secondSenType;
+
     this.thirdSen = thirdSen;
     this.fourthSen = fourthSen;
     this.fifthSen = fifthSen;
@@ -43,138 +47,175 @@ export default class TextAnimationWithMultipleSentences {
     this.generalSpeed = generalSpeed;
     this.generalTextColor = generalTextColor;
 
-    this.first = new TextAnimation(
-      this.firstSenType,
-      this.generalTextSize,
-      this.generalTextColor,
-      this.firstSen,
-      0,
-      0 * this.spacing,
-      this.generalSpeed
-    );
-    this.second = new TextAnimation(
-      this.secondSenType,
-      this.generalTextSize,
-      this.generalTextColor,
-      this.secondSen,
-      0,
-      1 * this.spacing,
-      this.generalSpeed
-    );
-    this.third = new TextAnimation(
-      this.generalTextFont,
-      this.generalTextSize,
-      this.generalTextColor,
-      this.thirdSen,
-      0,
-      2 * this.spacing,
-      this.generalSpeed
-    );
-    this.fourth = new TextAnimation(
-      this.generalTextFont,
-      this.generalTextSize,
-      this.generalTextColor,
-      this.fourthSen,
-      0,
-      3 * this.spacing,
-      this.generalSpeed
-    );
-    this.fifth = new TextAnimation(
-      this.generalTextFont,
-      this.generalTextSize,
-      this.generalTextColor,
-      this.fifthSen,
-      0,
-      4 * this.spacing,
-      this.generalSpeed
-    );
-    this.sixth = new TextAnimation(
-      this.generalTextFont,
-      this.generalTextSize,
-      this.generalTextColor,
-      this.sixthSen,
-      0,
-      5 * this.spacing,
-      this.generalSpeed
-    );
-    this.seventh = new TextAnimation(
-      this.generalTextFont,
-      this.generalTextSize,
-      this.generalTextColor,
-      this.seventhSen,
-      0,
-      6 * this.spacing,
-      this.generalSpeed
-    );
-    this.eighth = new TextAnimation(
-      this.generalTextFont,
-      this.generalTextSize,
-      this.generalTextColor,
-      this.eighthSen,
-      0,
-      7 * this.spacing,
-      this.generalSpeed
-    );
-    this.ninth = new TextAnimation(
-      this.generalTextFont,
-      this.generalTextSize,
-      this.generalTextColor,
-      this.ninthSen,
-      0,
-      8 * this.spacing,
-      this.generalSpeed
-    );
-    this.tenth = new TextAnimation(
-      this.generalTextFont,
-      this.generalTextSize,
-      this.generalTextColor,
-      this.tenthSen,
-      0,
-      9 * this.spacing,
-      this.generalSpeed
-    );
+    //creates a single text animation for the first sentence/first row
+    //y-value gets higher with every new row => arrange sentences below each other
+
+    this.first = 0;
+    this.second = 0;
+    this.third = 0;
+    this.fourth = 0;
+    this.fifth = 0;
+    this.sixth = 0;
+    this.seventh = 0;
+    this.eighth = 0;
+    this.ninth = 0;
+    this.tenth = 0;
+
+    this.sentenceNumberArray = [
+      this.first,
+      this.second,
+      this.third,
+      this.fourth,
+      this.fifth,
+      this.sixth,
+      this.seventh,
+      this.eighth,
+      this.ninth,
+      this.tenth,
+    ];
+
+    // this.second = new TextAnimation(
+    //   this.secondSenType,
+    //   this.generalTextSize,
+    //   this.generalTextColor,
+    //   this.secondSen,
+    //   0,
+    //   1 * this.spacing,
+    //   this.generalSpeed
+    // );
+    // this.third = new TextAnimation(
+    //   this.generalTextFont,
+    //   this.generalTextSize,
+    //   this.generalTextColor,
+    //   this.thirdSen,
+    //   0,
+    //   2 * this.spacing,
+    //   this.generalSpeed
+    // );
+    // this.fourth = new TextAnimation(
+    //   this.generalTextFont,
+    //   this.generalTextSize,
+    //   this.generalTextColor,
+    //   this.fourthSen,
+    //   0,
+    //   3 * this.spacing,
+    //   this.generalSpeed
+    // );
+    // this.fifth = new TextAnimation(
+    //   this.generalTextFont,
+    //   this.generalTextSize,
+    //   this.generalTextColor,
+    //   this.fifthSen,
+    //   0,
+    //   4 * this.spacing,
+    //   this.generalSpeed
+    // );
+    // this.sixth = new TextAnimation(
+    //   this.generalTextFont,
+    //   this.generalTextSize,
+    //   this.generalTextColor,
+    //   this.sixthSen,
+    //   0,
+    //   5 * this.spacing,
+    //   this.generalSpeed
+    // );
+    // this.seventh = new TextAnimation(
+    //   this.generalTextFont,
+    //   this.generalTextSize,
+    //   this.generalTextColor,
+    //   this.seventhSen,
+    //   0,
+    //   6 * this.spacing,
+    //   this.generalSpeed
+    // );
+    // this.eighth = new TextAnimation(
+    //   this.generalTextFont,
+    //   this.generalTextSize,
+    //   this.generalTextColor,
+    //   this.eighthSen,
+    //   0,
+    //   7 * this.spacing,
+    //   this.generalSpeed
+    // );
+    // this.ninth = new TextAnimation(
+    //   this.generalTextFont,
+    //   this.generalTextSize,
+    //   this.generalTextColor,
+    //   this.ninthSen,
+    //   0,
+    //   8 * this.spacing,
+    //   this.generalSpeed
+    // );
+    // this.tenth = new TextAnimation(
+    //   this.generalTextFont,
+    //   this.generalTextSize,
+    //   this.generalTextColor,
+    //   this.tenthSen,
+    //   0,
+    //   9 * this.spacing,
+    //   this.generalSpeed
+    // );
   }
 
   sentences() {
     push();
     translate(this.x, this.y);
 
-    this.first.typeWriter();
-
-    if (this.first.typingEnded === true) {
-      this.second.typeWriter();
+    for (this.k = 0; this.k < 10; this.k++) {
+      this.sentenceNumberArray[this.k] = new TextAnimation(
+        this.firstSenType,
+        this.generalTextSize,
+        this.generalTextColor,
+        this.firstSen,
+        0,
+        this.k * this.spacing,
+        this.generalSpeed
+      );
     }
 
-    if (this.second.typingEnded === true) {
-      this.third.typeWriter();
+    this.sentenceNumberArray[0].typeWriter();
+    for (this.i = 0; this.i < 10; this.i++) {
+      if (this.sentenceNumberArray[this.i].typingEnded === true) {
+        this.sentenceNumberArray[this.i + 1].typeWriter();
+      }
     }
 
-    if (this.third.typingEnded === true) {
-      this.fourth.typeWriter();
-    }
+    // this.first.typeWriter();
 
-    if (this.fourth.typingEnded === true) {
-      this.fifth.typeWriter();
-    }
+    // if (this.first.typingEnded === true) {
+    //   this.second.typeWriter();
+    // }
 
-    if (this.fifth.typingEnded === true) {
-      this.sixth.typeWriter();
-    }
+    // if (this.second.typingEnded === true) {
+    //   this.third.typeWriter();
+    // }
 
-    if (this.sixth.typingEnded === true) {
-      this.seventh.typeWriter();
-    }
+    // if (this.third.typingEnded === true) {
+    //   this.fourth.typeWriter();
+    // }
 
-    if (this.seventh.typingEnded === true) {
-      this.eighth.typeWriter();
-    }
+    // if (this.fourth.typingEnded === true) {
+    //   this.fifth.typeWriter();
+    // }
 
-    if (this.eighth.typingEnded === true) {
-      this.ninth.typeWriter();
-    }
-    if (this.ninth.typingEnded === true) {
-      this.tenth.typeWriter();
-    }
+    // if (this.fifth.typingEnded === true) {
+    //   this.sixth.typeWriter();
+    // }
+
+    // if (this.sixth.typingEnded === true) {
+    //   this.seventh.typeWriter();
+    // }
+
+    // if (this.seventh.typingEnded === true) {
+    //   this.eighth.typeWriter();
+    // }
+
+    // if (this.eighth.typingEnded === true) {
+    //   this.ninth.typeWriter();
+    // }
+    // if (this.ninth.typingEnded === true) {
+    //   this.tenth.typeWriter();
+    // }
 
     pop();
   }
