a840c5e3233b8a0256c7ce37849a257ef7dcc5f0
Simon de Vries <simon.vries@stud.h-da.de>
2021-06-16 20:37:28 +0200

    Update Pics

--- 01_Javascript/WeiterButton.js
+++ 01_Javascript/WeiterButton.js
@@ -16,9 +16,36 @@ export default class WeiterButton extends SpellButton {
       25,
       color(157, 157, 157),
       "WEITER",
-      this.x,
-      this.y,
+      this.x - 35,
+      this.y + 7,
       50
     );
+  }
+
+  designSleeping() {
+    tint(255, this.fadeInVariable);
+    image(
+      this.buttonSleeping,
+      this.x,
+      this.y,
+      this.buttonSleeping.width / 2,
+      this.buttonSleeping.height / 2
+    );
+    this.buttonText.textColor = color(157, 157, 157, this.fadeInVariable);
+  }
+
+  designActive() {
+    tint(255, this.fadeInVariable);
+    image(
+      this.buttonActive,
+      this.x,
+      this.y,
+      this.buttonActive.width / 2,
+      this.buttonActive.height / 2
+    );
+
+    image(this.buttonEffect, this.x, this.y, 250, 250);
+
+    this.buttonText.textColor = color(255, 236, 53, this.fadeInVariable);
   }
 }
